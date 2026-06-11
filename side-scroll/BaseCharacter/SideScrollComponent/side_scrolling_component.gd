344aedb887eccb7947642f7a0922671995cef54f
Waleed Aldawoud <waleed.s.d.09@gmail.com>
2026-06-11 14:13:33 +0300

    fixed movement

--- side-scroll/BaseCharacter/SideScrollComponent/side_scrolling_component.gd
+++ side-scroll/BaseCharacter/SideScrollComponent/side_scrolling_component.gd
@@ -5,6 +5,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var central_movement_component:CentralMovementComponent
 @export var movement_speed:float:
 	set(value):
+		movement_speed = value
 		if central_movement_component:
 			central_movement_component.set_vector("sidescrolling", Vector2(value,0))
 func _ready() -> void:
